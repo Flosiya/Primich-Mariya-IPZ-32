2980d039beaeb4de4c1e7946158f74b8a8631672
Flosiya <mashameky@gmail.com>
2023-12-02 10:33:06 +0200

    edit modal part

--- js/modal.js
+++ js/modal.js
@@ -1,32 +1,32 @@
 const modalController = () => {
-const buttonElem = document.querySelector('.hero-button');
-const modalElem = document.querySelector('.modal');
-
-modalElem.style.cssText = `
-display: flex;
-visibility: hidden;
-opacity:0;
-transition: opacity 300ms ease-in-out;
-`;
-
-const closeModal = event => {
-  const target = event.target;
-
-  if (target === modalElem || target.closest('.modal_close')){
-    
-    modalElem.style.opacity = 0;
-
-    setTimeout(() => {
-      modalElem.style.visibility = 'hidden';
-    }, 300)
+  const buttonElem = document.querySelector('.hero-button');
+  const modalElem = document.querySelector('.modal');
+  
+  modalElem.style.cssText = `
+  display: flex;
+  visibility: hidden;
+  opacity:0;
+  transition: opacity 300ms ease-in-out;
+  `;
+  
+  const closeModal = event => {
+    const target = event.target;
+  
+    if (target === modalElem || target.closest('.button-close')){
+      
+      modalElem.style.opacity = 0;
+  
+      setTimeout(() => {
+        modalElem.style.visibility = 'hidden';
+      }, 300)
+    }
   }
-}
-
-const openModal = () => {
-  modalElem.style.visibility = 'visible';
-  modalElem.style.opacity = 1;
-};
-buttonElem.addEventListener('click', openModal);
-modalElem.addEventListener('click', closeModal);
-};
-modalController();
+  
+  const openModal = () => {
+    modalElem.style.visibility = 'visible';
+    modalElem.style.opacity = 1;
+  };
+  buttonElem.addEventListener('click', openModal);
+  modalElem.addEventListener('click', closeModal);
+  };
+  modalController();
